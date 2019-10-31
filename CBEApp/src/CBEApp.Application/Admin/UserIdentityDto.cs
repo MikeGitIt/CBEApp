4c245cb90fb628912c24f1766ba57dbcbb71e34c
Mukesh Solanki <mukesh.solanki_contractor@jmfamily.com>
2019-10-31 17:59:13 +0530

    Added view, master layout and few references version are updated

---
+++ CBEApp/src/CBEApp.Application/Admin/UserIdentityDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBEApp.Admin
+{
+    public class UserIdentityDto
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PasswordText { get; set; }
+        
+
+        public int EventId { get; set; }
+        public string Access_Level_Code { get; set; }
+        public string RoleCode { get; set; }
+
+        public System.DateTime LastUpdatedDate { get; set; }
+        public string LastUpdatedUserId { get; set; }
+        public Nullable<System.DateTime> INCE_DT { get; set; }
+    }
+}
